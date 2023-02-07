5cd11d5e1eae4ff88b2a88ed1242cc9cedf138fc
mtorresinformatica <manueltorres.informatica@gmail.com>
2023-02-07 10:29:31 +0100

    Creación de la Interfaz y clase del servicio Empleado
    Se ha añadido en la clase principal el argumento (exclude={DataSourceAutoConfiguration.class}) en la anotación @SpringBootApplication, se ha modificado en el archivo application.properties, el nombre de la base de datos a dam y el valor de la acción sobre la persistencia a create-drop.
    Se ha creado la interfaz del servicio EmpleadoServiceI y la clase que implementa dicha interfaz EmpleadoServiceImpl.

--- src/main/java/com/example/proyecto/ProyectoApplication.java
+++ src/main/java/com/example/proyecto/ProyectoApplication.java
@@ -2,8 +2,9 @@ package com.example.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ProyectoApplication {
 
 	public static void main(String[] args) {
